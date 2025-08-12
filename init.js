2ff483779c3b0812bdb141770b36b80661acd107
TIConsil <ti@consilcontabilidade.com>
2025-08-12 13:24:29 -0300

    Dados atualizados - 12/08/2025, 13:24:28

--- init.js
+++ init.js
@@ -5,17 +5,11 @@ import gitPush from './js/gitPush.js';
 
 async function init() {
   try {
-    console.log('iniciou');
     await printScreenPlaywright();
-    console.log('rodou o printscreen');
     await delay(300);
-    console.log('esperou 300ms');
     await checkImageSize();
-    console.log('Alterou o json');
     await delay(500);
     await gitPush();
-    console.log('Fez o push');
-
   } catch (error) {
     console.log(error)
   }
